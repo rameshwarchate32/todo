77877c64b69577c2afe19263da8ee5e76d5a2000
rameshwarchate32 <rameshwarchate682@gmail.com>
2024-06-24 09:06:52 +0530

    first commit

---
+++ appointment/app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('add-todo/', views.add_todo, name='add_todo'),
+    path('delete-todo/<int:id>/', views.delete_todo, name='delete_todo'),
+    path('change-status/<int:id>/<str:status>/', views.change_todo, name='change_todo'),
+    path('logout/', views.signout, name='logout'),
+]
